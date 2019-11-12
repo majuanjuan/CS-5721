aa79692386d7bc35fffbc971c6718e94b7cc9ae4
curly <curlyjuanjuan@gmail.com>
2019-11-12 21:32:33 +0000

    add something in util

---
+++ Dealz4All/src/main/java/com/online/dealz/deal/util/Constants.java
@@ -0,0 +1,14 @@
+package com.online.dealz.deal.util;
+
+/**
+ * Description:
+ * @author curly
+ * @created 2019年11月12日
+ * This is a class for saving constants such as default values
+ */
+public class Constants {
+    /**
+     *
+     */
+    public static final String CURRENT_USER_ID = "CURRENT_USER_ID";
+}
